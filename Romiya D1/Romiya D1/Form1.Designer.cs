de977b82d0325999a0118cb4c8cf4dcdf9af0a85
romiyadangol <dangolromiya@gmail.com>
2022-11-22 09:43:10 +0545

    initial commit

--- Romiya D1/Romiya D1/Form1.Designer.cs
+++ Romiya D1/Romiya D1/Form1.Designer.cs
@@ -29,13 +29,91 @@ namespace Romiya_D1
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.username = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Age = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Font = new System.Drawing.Font("Sitka Small", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label1.Location = new System.Drawing.Point(47, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Name";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // username
+            // 
+            this.username.Location = new System.Drawing.Point(104, 55);
+            this.username.Name = "username";
+            this.username.Size = new System.Drawing.Size(143, 24);
+            this.username.TabIndex = 1;
+            this.username.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Book Antiqua", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(142, 115);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "submit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Age
+            // 
+            this.Age.AutoSize = true;
+            this.Age.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Age.Font = new System.Drawing.Font("Sitka Small", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Age.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Age.Location = new System.Drawing.Point(62, 85);
+            this.Age.Name = "Age";
+            this.Age.Size = new System.Drawing.Size(36, 21);
+            this.Age.TabIndex = 0;
+            this.Age.Text = "Age";
+            this.Age.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(104, 85);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(143, 24);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.username);
+            this.Controls.Add(this.Age);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox username;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label Age;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- Romiya D1/Romiya D1/Form1.cs
+++ Romiya D1/Romiya D1/Form1.cs
@@ -17,5 +17,41 @@ namespace Romiya_D1
             InitializeComponent();
         }
 
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // string data = username.Text;
+            // if(data == "romiya")
+            // {
+            // MessageBox.Show("Success");
+            // }
+            // else
+            // {
+            // MessageBox.Show("Failed");
+            // }
+            // for(int i=0;i<3;i++)
+            // {
+            // MessageBox.Show(data);
+            // MessageBox.Show("Display time : " + i);
+            try
+            {
+                int age_input = Convert.ToInt32(textBox1.Text);
+                MessageBox.Show("Age: " + age_input);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Invalid number:"+ex);
+            }
+            
+        }
     }
 }
